9362ccafef1ea83e517d9d2b34166a2f9cadcdde
jshih7 <8042780+jshih7@users.noreply.github.com>
2018-03-24 14:00:47 -0400

    Disabled timestamps when generating models or else sequelize will expect createdAt key in the table

--- generate-models.js
+++ generate-models.js
@@ -8,6 +8,9 @@ const options = {
 	host: "localhost",
     dialect: "mysql",
     directory: path.join(__dirname, "js/models"), // where to put models
+	additional: {
+		timestamps: false, // remove timestamps or sequelize expects createdAt field
+	},
 }
 
 const auto = new SequelizeAuto(database, username, password, options);
